efdb2678ae1957168d43038ba053e22aabbf3062
Alpha Diallo <dialloalpha5758@gmail.com>
2025-09-29 17:06:47 +0000

    first commit

---
+++ Assets/js/main.js
@@ -0,0 +1,22 @@
+
+const questions = [
+  {
+    question: "Quelle est la capitale de la France ?",
+    choices: ["Paris", "Rome", "Madrid", "Berlin"],
+    answer: "Paris"
+  },
+  {
+    question: "Quelle est la capitale du Japon ?",
+    choices: ["Tokyo", "Pékin", "Séoul", "Bangkok"],
+    answer: "Tokyo"
+  },
+  {
+    question: "Quelle est la capitale du Canada ?",
+    choices: ["Ottawa", "Toronto", "Montréal", "Vancouver"],
+    answer: "Ottawa"
+  }
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+
